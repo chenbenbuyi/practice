72da492f1dd61423918e00fb0797f273166dc420
chenbenbuyi <chenshaoxian_gtwx@163.com>
2020-11-21 00:34:22 +0800

    容器篇：容器填充、享元模式

--- src/main/java/thinckingInJava/part17/com/Pair.java
+++ src/main/java/thinckingInJava/part17/com/Pair.java
@@ -3,9 +3,12 @@ package thinckingInJava.part17.com;
 /**
  * @author chen
  * @date 2020/9/6 10:46
- * @Description Map 生成器接口
+ * @Description 用于Map的键值对结构
  */
 public class Pair<K,V> {
+    /**
+     *  final  定义是为了让对象实例为只读对象，一旦构建就不可更改
+     */
     public final K key;
     public final V v;
     public Pair(K key,V v) {
--- src/main/java/thinckingInJava/part17/p2/MapData.java
+++ src/main/java/thinckingInJava/part17/p2/MapData.java
@@ -1,15 +1,27 @@
 package thinckingInJava.part17.p2;
 
+import javafx.util.Pair;
 import thinckingInJava.part15_genericity.p3.Generator;
 
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.List;
 
 /**
  * @author chen
  * @date 2020/9/6 10:42
- * @Description
+ * @Description 使用Map适配器进行Map的元素填充
  */
 public class MapData<K, V> extends LinkedHashMap<K, V> {
+
+    public MapData(Generator<Pair<K, V>> gen, int quantity) {
+        for (int i = 0; i < quantity; i++) {
+            Pair<K, V> pair = gen.next();
+            put(pair.getKey(), pair.getValue());
+        }
+    }
+
 
     public MapData(Generator<K> genK, Generator<V> genV, int quantity) {
         for (int i = 0; i < quantity; i++) {
@@ -17,7 +29,44 @@ public class MapData<K, V> extends LinkedHashMap<K, V> {
         }
     }
 
+    public MapData(Generator<K> genK, V value, int quantity) {
+        for (int i = 0; i < quantity; i++) {
+            put(genK.next(), value);
+        }
+    }
+
+    public MapData(Iterable<K> genK, Generator<V> genV) {
+        for (K k : genK) {
+            put(k, genV.next());
+        }
+    }
+
+    public MapData(Iterable<K> genK, V value) {
+        for (K k : genK) {
+            put(k, value);
+        }
+    }
+
+    public static <K, V> MapData<K, V> map(Generator<Pair<K, V>> gen, int quantity) {
+        return new MapData<>(gen, quantity);
+    }
+
     public static <K, V> MapData<K, V> map(Generator<K> genK, Generator<V> genV, int quantity) {
         return new MapData<>(genK, genV, quantity);
     }
+
+    public static <K, V> MapData<K, V> map(Generator<K> genK, V value, int quantity) {
+        return new MapData<>(genK, value, quantity);
+    }
+
+    public static <K, V> MapData<K, V> map(Iterable<K> genK, Generator<V> genV) {
+        return new MapData<>(genK, genV);
+    }
+
+    public static <K, V> MapData<K, V> map(Iterable<K> genK, V value) {
+        return new MapData<>(genK, value);
+    }
+
 }
+
+
