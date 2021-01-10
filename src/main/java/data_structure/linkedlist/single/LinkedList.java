4010cf18daac419224a0719dd578b06ff887f3a5
chenbenbuyi <chenshaoxian_gtwx@163.com>
2021-01-10 23:37:30 +0800

    偷懒了，只添加了effective 的部分章节目录

--- src/main/java/data_structure/linkedlist/single/LinkedList.java
+++ src/main/java/data_structure/linkedlist/single/LinkedList.java
@@ -6,7 +6,7 @@ package data_structure.linkedlist.single;
  *  聊表由于指针（引用）的链式逻辑关系，在写的时候很容易绕晕。画图是最好的辅助手段
  *  此外，多测试检查一些边界情况，如：
  *  链表为空
- *  只有一个结点或两个结点
+ *  只有一个或两个结点
  *  头结点和尾结点
  */
 public class LinkedList {
