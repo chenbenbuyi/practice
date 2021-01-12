c7c9fb206e6f987fb533350004cfd01704d00b6f
chenbenbuyi <chenshaoxian_gtwx@163.com>
2021-01-12 23:18:36 +0800

    可见性测试

---
+++ src/main/java/java_concurrency/chapter3/NoVisibility.java
@@ -0,0 +1,28 @@
+package java_concurrency.chapter3;
+
+/**
+ * @author chen
+ * @date 2021/1/12 22:14
+ * @Description 可见性测试
+ */
+public class NoVisibility {
+    private static boolean ready;
+    private static int number;
+
+    private static class MyThread extends Thread {
+        @Override
+        public void run() {
+            while (!ready) {
+                Thread.yield();
+            }
+            System.out.println(number);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Thread(new MyThread()).start();
+        number = 100;
+        ready = true;
+    }
+
+}
