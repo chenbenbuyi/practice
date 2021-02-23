d4cbe594c0f711c9c53ad00562c04c0a977cb08d
chenbenbuyi <chenshaoxian_gtwx@163.com>
2021-02-23 22:07:06 +0800

    HashMap 的几种常见迭代方式和线程池关闭方法的区别

---
+++ src/main/java/collection/map/MapIterationTest.java
@@ -0,0 +1,55 @@
+package collection.map;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author chen
+ * @date 2021/2/23 7:29
+ * @Description 问： HashMap有多少种迭代方式,下面会告诉你答案
+ * 参考：https://mp.weixin.qq.com/s/Zz6mofCtmYpABDL1ap04ow
+ */
+public class MapIterationTest {
+    static Map<Integer, String> map;
+
+    static {
+        map = new HashMap<>();
+        map.put(1, "Java");
+        map.put(2, "JDK");
+        map.put(3, "Spring Framework");
+        map.put(4, "MyBatis framework");
+        map.put(5, "Java中文社群");
+    }
+
+    public static void main(String[] args) {
+        // 1 迭代器——利用 entrySet 或 keySet
+        Set<Map.Entry<Integer, String>> entries = map.entrySet();
+        Iterator<Map.Entry<Integer, String>> iterator = entries.iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<Integer, String> entry = iterator.next();
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+        // foreach——利用 entrySet 或 keySet
+        Set<Map.Entry<Integer, String>> entries2 = map.entrySet();
+        for (Map.Entry<Integer, String> entry : entries2) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+
+        // Lambda 表达式遍历
+        map.forEach((key, value) -> {
+            System.out.println(key + ":" + map.get(key));
+        });
+
+        // Stream 遍历
+        map.entrySet().stream().forEach((entry)->{
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        });
+
+        // Stream 多线程遍历——其底层使用Fork/Join框架实现
+        map.entrySet().parallelStream().forEach((entry)->{
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        });
+    }
+}
--- src/main/java/java_concurrency/chapter6/ExecutorServiceTest.java
+++ src/main/java/java_concurrency/chapter6/ExecutorServiceTest.java
@@ -24,7 +24,7 @@ public class ExecutorServiceTest {
             tasks.add(callable);
         }
         /**
-         *  invokeAll 执行所有提交的任务集合，当所有任务都完成或调用线程中断或超时时，该方法将返回。如果超时后还有任务为执行，未完成的任务将被取消
+         *  invokeAll 执行所有提交的任务集合，当所有任务都完成或调用线程中断或超时时，该方法将返回。如果超时后还有任务未执行，未完成的任务将被取消
          *  任务按照添加任务集合中迭代器的顺序返回。所以，这依赖于具体容器的迭代顺序
          */
         List<Future<String>> futures = executor.invokeAll(tasks);
@@ -33,5 +33,20 @@ public class ExecutorServiceTest {
             System.out.println(future.get());
         }
         executor.shutdown();
+        /**
+         * shutdown() 关闭了提交通道，用submit()是无效的；而内部该怎么跑还是怎么跑，跑完再停。
+         * shutdownNow() 能立即停止线程池，正在跑的和正在等待的任务都停下了。这样做立即生效，风险也比较大；
+         * awaitTermination(long timeOut, TimeUnit unit) 会使当前线程阻塞，直到
+         * 1.等所有已提交的任务（包括正在跑的和队列中等待的）执行完
+         * 2.或者等超时时间到
+         * 3.或者线程被中断，抛出InterruptedException
+         *
+         * shutdown()和 awaitTermination() 的区别
+         * shutdown()后，不能再提交新的任务进去；但是awaitTermination()后，可以继续提交。
+         * awaitTermination()是阻塞的，返回结果是线程池是否已停止（true/false）；shutdown()不阻塞。
+         * awaitTermination 并不是用来关闭线程池，它只是用来检测 timeout 时间后线程池是否关闭。 一般在调用shutdown()方法后调用
+         * 所以，awaitTermination 使用之前 必须先手动关闭线程池，否则一直会阻塞当前线程直到超时为止
+         */
+        executor.awaitTermination(5, TimeUnit.SECONDS);
     }
 }
