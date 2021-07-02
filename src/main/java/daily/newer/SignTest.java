f6ff5160d47ff0117ca5da9a6e79e87e179dc207
chenshaoxian <chenshaoxian_gtwx@163.com>
2021-07-02 19:22:15 +0800

    密钥对生成测试

---
+++ src/main/java/daily/newer/SignTest.java
@@ -0,0 +1,122 @@
+package daily.newer;
+
+import cn.hutool.core.util.HexUtil;
+import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
+import org.bouncycastle.crypto.generators.ECKeyPairGenerator;
+import org.bouncycastle.crypto.params.ECDomainParameters;
+import org.bouncycastle.crypto.params.ECKeyGenerationParameters;
+import org.bouncycastle.crypto.params.ECPrivateKeyParameters;
+import org.bouncycastle.crypto.params.ECPublicKeyParameters;
+import org.bouncycastle.math.ec.ECCurve;
+import org.bouncycastle.math.ec.ECFieldElement;
+import org.bouncycastle.math.ec.ECPoint;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+/**
+ * @date: 2021/7/1 13:49
+ * @author: chen
+ * @desc:
+ */
+public class SignTest {
+    public static final BigInteger SM2_ECC_P = new BigInteger("FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFF",16);
+
+    public static final BigInteger SM2_ECC_A = new BigInteger("FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFC",16);
+
+    public static final BigInteger SM2_ECC_B = new BigInteger("28E9FA9E9D9F5E344D5A9E4BCF6509A7F39789F515AB8F92DDBCBD414D940E93",16);
+
+    public static final BigInteger SM2_ECC_N = new BigInteger("FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFF7203DF6B21C6052B53BBF40939D54123",16);
+
+    public static final BigInteger SM2_ECC_GX = new BigInteger("32C4AE2C1F1981195F9904466A39C9948FE30BBFF2660BE1715A4589334C74C7",16);
+
+    public static final BigInteger SM2_ECC_GY = new BigInteger("BC3736A2F4F6779C59BDCEE36B692153D0A9877CC62A474002DF32E52139F0A0",16);
+
+
+
+    public static void main(String[] args) {
+        ECKeyPairGenerator keyPairGenerator = new ECKeyPairGenerator();
+        ECFieldElement.Fp gxFieldElement = new ECFieldElement.Fp(SM2_ECC_P, SM2_ECC_GX);
+        ECFieldElement.Fp gyFieldElement = new ECFieldElement.Fp(SM2_ECC_P, SM2_ECC_GY);
+        ECCurve.Fp curve = new ECCurve.Fp(SM2_ECC_P, SM2_ECC_A, SM2_ECC_B);
+        ECPoint.Fp pointG =new ECPoint.Fp(curve, gxFieldElement, gyFieldElement);
+        ECDomainParameters domainParams = new ECDomainParameters(curve, pointG, SM2_ECC_N);
+
+        ECKeyGenerationParameters keyGenerationParams = new ECKeyGenerationParameters(domainParams, new SecureRandom());
+        keyPairGenerator.init(keyGenerationParams);
+        AsymmetricCipherKeyPair keyPair = keyPairGenerator.generateKeyPair();
+        ECPrivateKeyParameters privateKeyParams = (ECPrivateKeyParameters)keyPair.getPrivate();
+        ECPublicKeyParameters publicKeyParams = (ECPublicKeyParameters)keyPair.getPublic();
+        BigInteger privateKey = privateKeyParams.getD();
+        ECPoint publicKey = publicKeyParams.getQ();
+        System.out.println("len:" + privateKey.toByteArray().length);
+        KeyPair generateKeyPair = new KeyPair(publicKey, privateKey);
+        byte[] pub = formatPublicKey(generateKeyPair.getPublicKey());
+        byte[] pri = formatPrivateKey(generateKeyPair.getPrivateKey());
+        System.out.println("公钥Base64编码："+Base64Utils.encodeToString(pub));
+        System.out.println("私钥Base64编码："+Base64Utils.encodeToString(pri));
+
+        System.out.println("公钥16进制编码："+HexUtil.encodeHexStr(pub));
+        System.out.println("私钥16进制编码："+HexUtil.encodeHexStr(pri));
+
+    }
+
+    public static class KeyPair
+    {
+        private ECPoint publicKeyEc;
+        private BigInteger privateKeyBi;
+
+        public KeyPair(ECPoint publicKey, BigInteger privateKey)
+        {
+            this.publicKeyEc = publicKey;
+            this.privateKeyBi = privateKey;
+        }
+
+        public ECPoint getPublicKeyEc() {
+            return this.publicKeyEc;
+        }
+
+        public BigInteger getPrivateKeyBi() {
+            return this.privateKeyBi;
+        }
+
+        public byte[] getPrivateKey() {
+            if (this.privateKeyBi != null) {
+                return CommonUtils.byteConvert32Bytes(this.privateKeyBi);
+            }
+            return new byte[0];
+        }
+
+        public byte[] getPublicKey() {
+            if (this.publicKeyEc != null) {
+                return this.publicKeyEc.getEncoded();
+            }
+            return new byte[0];
+        }
+    }
+
+    private static byte[] formatPrivateKey(byte[] key) {
+        if (key.length == 32) {
+            return key;
+        }
+        if (key.length == 33) {
+            byte[] pk = new byte[32];
+            System.arraycopy(key, 1, pk, 0, 32);
+            return pk;
+        }
+        throw new RuntimeException("private key length error");
+    }
+
+    private static byte[] formatPublicKey(byte[] key) {
+        if (key.length == 64) {
+            return key;
+        }
+        if (key.length == 65) {
+            byte[] pk = new byte[64];
+            System.arraycopy(key, 1, pk, 0, pk.length);
+            return pk;
+        }
+        throw new RuntimeException("private key length error");
+    }
+
+}
--- src/main/java/data_structure/stack/StackTest.java
+++ src/main/java/data_structure/stack/StackTest.java
@@ -1,5 +1,7 @@
 package data_structure.stack;
 
+import com.google.common.collect.ImmutableMap;
+
 import java.util.*;
 
 /**
@@ -9,43 +11,42 @@ import java.util.*;
  * 给定 ( ) {}  [] 组成的的字符串，判断是否有效——左括号必须有相同类型的右括号闭合，左括号必须以正确的顺序闭合
  * 如 "()"  "()[]" "{[]}"都是有效的
  * 除此之外，栈还可以应用在比如浏览器前进回退、字符串反转等实际场景，当然，方法调用肯定是栈来实现的
- *
  */
 public class StackTest {
+
+    public static Map<Character, Character> map = ImmutableMap.<Character, Character>builder()
+            .put('(', ')')
+            .put('[', ']')
+            .put('{', '}')
+            .build();
+
     public static void main(String[] args) {
         boolean res = isValid("]");
         System.out.println(res);
     }
 
-
     /**
      * 思路：
      * 创建一个栈。遍历字符串，如果字符是左括号就直接加入stack中，否则将 stack 的栈顶元素与这个括号做比较，如果不相等就直接返回 false。遍历结束，如果stack为空，返回 true。
-     *
+     * <p>
      * 代码编写注意考虑异常极端情况，比如栈为空时弹栈会不会异常？
      */
     private static boolean isValid(String s) {
-        HashMap<Character, Character> map = new HashMap<>(16);
         // 栈既可以通过数组实现，也可以通过链表来实现。不管基于数组还是链表，入栈、出栈的时间复杂度都为 O(1)。此处的Stack是继承于 Vector 的同步容器
         Stack<Character> stack = new Stack<>();
-        map.put('(', ')');
-        map.put('[', ']');
-        map.put('{', '}');
         char[] chars = s.toCharArray();
         for (Character c : chars) {
-            if(map.containsKey(c)){
+            if (map.containsKey(c)) {
                 stack.push(c);
-            }else {
+            } else {
                 Character top = stack.isEmpty() ? '#' : stack.pop();
-                if(!Objects.equals(map.get(top),c)){
+                if (!Objects.equals(map.get(top), c)) {
                     return false;
                 }
             }
         }
         return stack.isEmpty();
     }
-
-
 
 
 }
